d8f72d48e0a10500eeb297880aaa3f4fcf8a8b9e
Mathis Bullinger <mathis@bullinger.dev>
2020-11-28 15:22:57 +0100

    feat: set ws endpoint

--- src/handler.ts
+++ src/handler.ts
@@ -1,12 +1,14 @@
 import type { APIGatewayEvent } from 'aws-lambda'
-import { send } from '~/utils/ws'
+import ws from '~/utils/ws'
 
 export const socket = async (event: APIGatewayEvent) => {
   console.log(`\n\n\n==${event.requestContext.eventType}==\n\n`)
 
   if (event.requestContext.eventType === 'MESSAGE')
-    await send(event.requestContext.connectionId, {
+    await ws(event).send(event.requestContext.connectionId, {
       message: 'got a message',
       original: event.body,
     })
+
+  return { statusCode: 200 }
 }
--- src/utils/ws.ts
+++ src/utils/ws.ts
@@ -1,16 +1,21 @@
 import AWS from 'aws-sdk'
+import type { APIGatewayEvent } from 'aws-lambda'
 
-export const gateway = new AWS.ApiGatewayManagementApi({
-  apiVersion: '2018-11-29',
-  endpoint: process.env.IS_OFFLINE
-    ? 'http://localhost:3001'
-    : process.env.GATEWAY,
-})
+export default ({ requestContext }: APIGatewayEvent) => {
+  const gateway = new AWS.ApiGatewayManagementApi({
+    apiVersion: '2018-11-29',
+    endpoint: process.env.IS_OFFLINE
+      ? 'http://localhost:3001'
+      : `${requestContext.domainName}/${requestContext.stage}`,
+  })
 
-export const send = async (ConnectionId: string, data: unknown) =>
-  await gateway
-    .postToConnection({
-      ConnectionId,
-      Data: typeof data === 'string' ? data : JSON.stringify(data),
-    })
-    .promise()
+  const send = async (ConnectionId: string, data: unknown) =>
+    await gateway
+      .postToConnection({
+        ConnectionId,
+        Data: typeof data === 'string' ? data : JSON.stringify(data),
+      })
+      .promise()
+
+  return { send }
+}
