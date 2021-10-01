460965d62555420d31e6a6e4edd757458609f052
Mathis Bullinger <mathis@bullinger.dev>
2021-10-01 18:58:53 +0200

    fix: retry count per task

--- src/utils/backoff.ts
+++ src/utils/backoff.ts
@@ -7,21 +7,19 @@ export const backoff = <T extends λ>(
   task: T,
   shouldRetry: (err: unknown, delay: number) => boolean = (_, ms) => ms < 1000
 ) => {
-  let retries = 0
-
   const attempt = async (
+    retries: number,
     ...args: Parameters<T>
   ): Promise<PromType<ReturnType<T>>> => {
     try {
       return await task(...args)
     } catch (err) {
-      const delay = 2 ** retries++ * 100
+      const delay = 2 ** retries * 100
       if (!shouldRetry(err, delay)) throw err
       console.log(`retry (${retries}) after ${delay} ms`)
       await sleep(delay)
-      return await attempt(...args)
+      return await attempt(retries + 1, ...args)
     }
   }
-
-  return attempt
+  return async (...args: Parameters<T>) => await attempt(0, ...args)
 }
